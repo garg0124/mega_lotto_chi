76596677691edb3fc788cdc5ed3ff04bccae9024
Manasvi Garg <manasvi.garg@unthinkable.co>
2022-07-12 12:49:51 +0530

    Added implementation for gem

---
+++ spec/mega_lotto_chi/drawing_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+module MegaLottoChi
+  RSpec.describe Drawing do
+    describe "#draw" do
+      let(:drawing) { Drawing.new.draw }
+
+      it "returns an array" do
+        expect(drawing).to be_a(Array)
+      end
+
+      it "returns an array with 5 element" do
+        expect(drawing.size).to eq(5)
+      end
+
+      it "each element is an Integer" do
+        drawing.each do |item|
+         expect(item).to be_a(Integer)
+        end
+      end
+
+      it "each element is < 60" do
+        drawing.each do |item|
+         expect(item).to be < 60
+        end
+      end
+    end
+  end
+end
