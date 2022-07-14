12584aa692ead3deb4f798344cccadd3618c7e0b
Manasvi Garg <manasvi.garg@unthinkable.co>
2022-07-14 15:23:05 +0530

    updated files

--- lib/mega_lotto_chi.rb
+++ lib/mega_lotto_chi.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "./mega_lotto_chi/version"
-require_relative "./mega_lotto_chi/drawing"
-require_relative "./mega_lotto_chi/core_ext"
+require "mega_lotto_chi/version"
+require "mega_lotto_chi/drawing"
+require "mega_lotto_chi/core_ext"
 
 begin
   require "pry"
